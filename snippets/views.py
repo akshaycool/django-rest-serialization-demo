ca6068e54c8e0365cdfa08b39245948cbe6b29bc
Akshay Shah <akshay.vjti11@gmail.com>
2016-09-09 00:26:59 +0530

    Initial commit

---
+++ snippets/views.py
@@ -0,0 +1,60 @@
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+@csrf_exempt
+def snippet_list(request):
+        if request.method == 'GET':
+            snippets=Snippet.objects.all()
+            serializer=SnippetSerializer(snippets,many=True)
+            return JsonResponse(serializer.data)
+
+        if request.method == 'POST':
+            data= JSONParser().parse(request)
+            serializer=SnippetSerializer(data=data)
+            if(serializer.is_valid):
+                serializer.save()
+                return JsonResponse(serializer.data,status=201)
+            return JsonResponse(serializer.erros,status=400)
+
+
+#View for CRUD operations of Snippet
+"""
+Algorithm
+    1) Check if the snippet is present against primary key
+    2) Depending on the request type perfom the operation
+        GET- Get The Snippet .Serialize it.Return the JSON Response
+        PUT(Update)- Get the Snippet from the request. Deserialize it.
+        DELETE - Delete the snippet.
+
+
+
+"""
+@csrf_exempt
+def snippet_detail(request,pk):
+        try:
+            snippet=Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            return HttpResponse(status=404)
+
+        if request.method == 'GET':
+            serializer=SnippetSerializer(snippet)
+            return JsonResponse(serializer.data)
+
+        elif request.method == 'PUT':
+            data-JSONParser().parse(snippet)
+            serializer=SnippetSerializer(data=data)
+            if serializer.is_valid:
+                snippet.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors,status=400)
+        elif request.method == 'DELETE':
+            snippet.delete()
+            return HttpResponse(status=204)
