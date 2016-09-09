c223c61264429129126f72cafa750a2484522737
Akshay Shah <akshay.vjti11@gmail.com>
2016-09-09 13:59:26 +0530

    adding code for snippet modification only by creator

---
+++ snippets/permissions.py
@@ -0,0 +1,17 @@
+#Object Level Permissions
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Custom permission to only allow owners of an object to modify it
+    """
+
+    def has_object_permission(self,request,view,obj):
+        #Read permissions always allow GET,HEAD or OPTIONS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+
+        #Write permissions are only allowed for owner of the snippet
+        return obj.owner == request.user
+
+
